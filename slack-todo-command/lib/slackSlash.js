dc5b7c223eb58945b5438c42d822ac9e7f5ec553
Khanhhv <khanhhv@ants.vn>
2018-10-18 00:20:46 +0700

    update

---
+++ slack-todo-command/lib/slackSlash.js
@@ -0,0 +1,116 @@
+// This file is suchly named because the original intention was to make this handle all sorts of slash-related extra functionality, not just/todo.
+
+var fs = require('fs');
+var Q = require('q');
+var _ = require('lodash');
+
+var defaultText = fs.readFileSync(__dirname+'/slackSlashTodoHelp.txt', {encoding: 'utf8'});
+
+// Retrieve
+var MongoClient = require('mongodb').MongoClient;
+
+var dbUp = Q.ninvoke(MongoClient, 'connect', 'mongodb://localhost:27017/todos'); // Returns mongodb as a promise.
+
+module.exports = function (slashObject) {
+  var deferred = Q.defer();
+
+  // Extract command from the data string.
+  slashObject.todoData = slashObject.text.split(' ');
+  slashObject.todoCommand = slashObject.todoData.shift();
+  slashObject.todoData = slashObject.todoData.join(' ');
+
+  switch (slashObject.todoCommand) {
+    case 'add':
+      deferred.resolve(todoAdd(slashObject));
+      break;
+    case 'show':
+    case 'list':
+    case 'ls':
+      deferred.resolve(todoList(slashObject));
+      break;
+    case 'clear':
+    case 'x':
+      deferred.resolve(todoClear(slashObject));
+      break;
+    case 'help':
+      deferred.resolve(defaultText);
+      break;
+    default:
+      if (slashObject.todoData.trim().length > 0) {
+        slashObject.todoData = slashObject.todoCommand+' '+slashObject.todoData;
+        deferred.resolve(todoAdd(slashObject));
+      } else {
+        deferred.resolve(todoList(slashObject));
+      }
+  }
+
+  return deferred.promise;
+};
+
+function todoAdd(s) {
+    console.log({
+      user: s.user_name,
+      todo: s.todoData,
+      createdAt: new Date()
+    });
+
+  return dbUp.then(function (db) {
+    return Q.ninvoke(db.collection('todos'), 'insert', {
+      user: s.user_name,
+      todo: s.todoData,
+      createdAt: new Date()
+    });
+  }).then(function (docs) {
+    return 'Added todo (type \'/todo\' to show).';
+});
+}
+
+function todoList(s) {
+    console.log('hihihihihi', dbUp);
+  var response = {
+    data: '',
+    append: function (str) {
+      response.data += str+'\n';
+    }
+  };
+
+  return dbUp.then(function (db) {
+    return Q.ninvoke(db.collection('todos').find(), 'toArray');
+  }).then(function (todos) {
+    usersTodos = _.groupBy(todos, function (todo) {
+      return todo.user;
+    });
+    console.log(usersTodos);
+    _.forOwn(usersTodos, function (todos, userName) {
+      response.append('*'+userName+':*');
+      todos.forEach(function (todo, index) {
+        response.append(''+(index + 1)+'. '+todo.todo);
+      });
+      response.append('');
+    });
+    response.append('*/todo x <number>* clears a task. */todo help* shows available commands.');
+    return response.data;
+  });
+}
+
+function todoClear(s) {
+  var db, targetUserTodos, targetTodo;
+  return dbUp.then(function (x) {
+    db = x;
+    return Q.ninvoke(db.collection('todos').find(), 'toArray');
+  }).then(function (todos) {
+    usersTodos = _.groupBy(todos, function (todo) {
+      return todo.user;
+    });
+    var targetIndex = parseInt(s.todoData.trim(), 10) - 1;
+    if (_.isNaN(targetIndex)) throw new Error('Please enter a numeric todo ID (0-9).');
+    targetUserTodos = usersTodos[s.user_name] || [];
+    targetTodo = targetUserTodos[targetIndex];
+    if (!(targetTodo || {})._id) throw new Error('You don\'t have a todo with id '+(targetIndex+1)+'!');
+    return Q.ninvoke(db.collection('todos'), 'remove', {_id: targetTodo._id}, 1);
+  }).then(function (docs) {
+    var qty = targetUserTodos.length - 1 > 0 ? targetUserTodos.length - 1 : 'no';
+    var todoPlural = targetUserTodos.length === 2 ? 'todo' : 'todos';
+    return 'Cleared \''+targetTodo.todo+'\'. You have '+qty+' '+todoPlural+' left.';
+  });
+}
---
+++ slack-todo-command/server.js
@@ -0,0 +1,54 @@
+var http = require('http');
+var querystring = require('querystring');
+var Q = require('q');
+
+// App brains.
+var slackSlash = require('./lib/slackSlash.js');
+
+var MongoClient = require('mongodb').MongoClient;
+
+
+
+
+
+var port = 5000;
+var server = http.createServer(function (request, response) {
+  var reqInfoPrefix = request.method+' '+request.url+' : '; // For logging.
+
+  // Haystakt slash commands for Slack.
+  // curl -d {} http://localhost:5000/slack/slash
+  if (request.method === 'POST' && request.url === '/slack/slash') {
+    Q().then(function () {
+      var deferred = Q.defer();
+      var body = '';
+      request.on('data', function (chunk) {body += chunk;});
+      request.on('end', function () {
+        var data = querystring.parse(body);
+        reqInfoPrefix += data.command+' ';
+        deferred.resolve(data);
+      });
+      return deferred.promise;
+    }).then(slackSlash).then(
+      // Final success.
+      function (data) {
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.end(data);
+        console.log(reqInfoPrefix+'served.');
+      },
+      // Final error handler.
+      function (err) {
+        response.writeHead(400);
+        response.end(err.message || err);
+        console.log(reqInfoPrefix+'Error: '+(err.message || err));
+      }
+    );
+
+  } else {
+    response.writeHead(404);
+    response.write('404: Nothing Here.\n');
+    response.end();
+    console.log(reqInfoPrefix+'Rejected (404).');
+  }
+});
+
+server.listen(port);
